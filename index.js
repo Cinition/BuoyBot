e731adf7addabb351f2e45eb39b79b697a9fb5cc
Cornee Nieuwenhuis <0301168@student.rocvantwente.nl>
2020-05-21 17:45:28 +0200

    Beginning of vövve bot

---
+++ index.js
@@ -0,0 +1,15 @@
+const { prefix, token} = require('./settings.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLocaleLowerCase() === 'pat') {
+    msg.reply('Bork!');
+  }
+});
+
+client.login(token);
